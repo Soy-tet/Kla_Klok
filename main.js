742a0d26912b8400e76954dbd9ee946990e362b1
Soy Vitou <soyvitou999@gmail.com>
2023-03-18 09:57:52 +0700

    1.0.0

---
+++ main.js
@@ -0,0 +1,10 @@
+function random(){
+    var tiger = document.getElementById("tiger");
+    var guard = document.getElementById("guard");
+    var chicken = document.getElementById("chicken");
+    var loster = document.getElementById("loster");
+    var crab = document.getElementById("crab");
+    var fish = document.getElementById("fish");
+    var array = ["tiger","guard", "chicken", "loster", "crab","fish"];
+    var select = math.floor(math.random());
+}
